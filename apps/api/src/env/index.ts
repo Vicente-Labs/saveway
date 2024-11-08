525b70cd6bd7eeec6f70b38ef20b1c1ed356be77
Vicentesan <vikom.sanchez@gmail.com>
2024-11-08 11:08:18 -0300

    feat: init drizzle and add base user tables

--- apps/api/src/env/index.ts
+++ apps/api/src/env/index.ts
@@ -1,8 +1,9 @@
-import z from "zod"
+import z from 'zod'
 
 const envSchema = z.object({
   JWT_SECRET: z.string(),
   BACKEND_PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
